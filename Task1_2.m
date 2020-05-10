cc74838514fa6e8cfc559d8ef64e17c415025f37
yy5519 <yy5519@imperial.ac.uk>
2020-05-10 17:17:41 +0800

    separate files in different folders

--- Task1_2.m
+++
@@ -1,7 +0,0 @@
-x = 1.4;
-reference = sin(x);
-result = 0;
-for i = 0:5
-    result = result + ((-1)^i)*(x^(2*i+1))/(factorial(2*i+1));
-end
-diff = reference - result;
--- Task1_3.m
+++
@@ -1,9 +0,0 @@
-x = (1+1i)/sqrt(2); % try to find the cube root
-argument = atan(1/1);
-amplitude = sqrt(abs(real(x))^2+abs(imag(x))^2);
-cube_amplitude=amplitude ^ (1/3);
-for i = 0:2
-    answer = cube_amplitude * exp(1j*(argument*(1/3)+2*pi*i/3));
-    format short;
-    disp(answer);
-end
