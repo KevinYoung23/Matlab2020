d91a10d78ce997ee443667a6642608798b02bce6
yy5519 <yy5519@imperial.ac.uk>
2020-05-11 11:20:48 +0800

    Example2_4v2

--- Lecture2/Example2_4.m
+++ Lecture2/Example2_4.m
@@ -2,5 +2,8 @@ x = linspace(-2, 2, 101);
 y = linspace(-2, 2, 101);
 [X, Y] = meshgrid(x, y);
 Z = exp(-(X.^2 + Y.^2));
+%construct an array of levels 
+l = linspace(0, 1, 20)
 figure
-contour(X, Y, Z)
+contourf(X, Y, Z, l, 'Linecolor', 'none')
+title('f(x, y) = exp(-(x^2 + y^2))')
